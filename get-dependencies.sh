9ed53a5644fce6382ef8f54a3d1aa60632cd1334
juju4 <juju4@users.noreply.github.com>
2017-01-07 04:23:17 -0500

    sync repositories - add Jenkinsfile

---
+++ get-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+if [ $# != 0 ]; then
+rolesdir=$1
+else
+rolesdir=$(dirname $0)/..
+fi
+
+#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.icinga2 ] && ln -s ansible-icinga2 $rolesdir/juju4.icinga2
+[ ! -e $rolesdir/juju4.icinga2 ] && cp -R $rolesdir/ansible-icinga2 $rolesdir/juju4.icinga2
+
+## don't stop build on this script return code
+true
+
--- test/integration/default/serverspec/pnp4nagios_spec.rb
+++ test/integration/default/serverspec/pnp4nagios_spec.rb
@@ -12,4 +12,12 @@ end
 describe package('pnp4nagios-web'), :if => os[:family] == 'ubuntu' && (os[:release] == '14.04' || os[:release] == '12.04') do
   it { should be_installed }
 end
+describe command('curl -sSqL http://localhost/pnp4nagios/'), :if => os[:family] == 'ubuntu' && (os[:release] == '14.04' || os[:release] == '12.04') do
+## may not be present if no data retrieved
+#  its(:stdout) { should match /<title>Service overview for/ }
+  its(:stdout) { should match /<meta http-equiv="refresh" content=".*"; URL="\/pnp4nagios\/graph/ }
+  its(:stdout) { should match /pnp4nagios/ }
+  its(:stdout) { should_not match /404 Not Found/ }
+  its(:exit_status) { should eq 0 }
+end
 
